69b8a2242c04cb606bb1c8395542ba927a591bd9
gilardh <gilardh@yahoo.com>
2019-06-08 01:14:56 +0200

    Update bitcointalk link

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -65,7 +65,7 @@ export default class ZContact extends React.Component {
                     <a ><Invisible size='12'/></a>
                     <a href="https://reddit.com/r/Myhush"><Reddit size='40'/></a>
                     <a ><Invisible size='12'/></a>
-                    <a href="https://bitcointalk.org/index.php?topic=2008578.0"><Bitcointalk size='40'/></a>
+                    <a href="https://bitcointalk.org/index.php?topic=2827088.0"><Bitcointalk size='40'/></a>
                     <a ><Invisible size='12'/></a>
                     <a href="https://github.com/bitcoinhush"><Github size='40'/></a>
                   </p>

68d6d65aa735f5a81a8fecc7c7d9e32092ed4728
gilardh <gilardh@yahoo.com>
2020-06-17 00:06:57 +0200

    Updates

---
+++ precache-manifest.97a8bf0209f0d84678534bb8bd6f6893.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0cce020b7ec52f6d73bf",
+    "url": "/myhushwallet/static/css/main.83f89ca0.chunk.css"
+  },
+  {
+    "revision": "0cce020b7ec52f6d73bf",
+    "url": "/myhushwallet/static/js/main.1798a46f.chunk.js"
+  },
+  {
+    "revision": "611d97b05c4875bc9c51",
+    "url": "/myhushwallet/static/js/runtime~main.e4680a7d.js"
+  },
+  {
+    "revision": "8c8ff005b2993db5771c",
+    "url": "/myhushwallet/static/css/2.6932d955.chunk.css"
+  },
+  {
+    "revision": "8c8ff005b2993db5771c",
+    "url": "/myhushwallet/static/js/2.2f400369.chunk.js"
+  },
+  {
+    "revision": "d1ec5a96748c78774f8b7358da50b8e0",
+    "url": "/myhushwallet/index.html"
+  }
+];
